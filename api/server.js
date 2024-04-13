6979f45c33c2e301795c1ab15bdafb8557145c88
Alloush95 <mohamad.alloush0090@stud.hkr.se>
2024-04-13 15:50:20 +0200

    connect to DB

--- api/server.js
+++ api/server.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
 const port = 3000;
 const app = express()
+const passWord = process.env.PASS_WORD; // get the password from the .env file
 
+// Connect to MongoDB
+mongoose.connect(passWord, {useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("connected to MongoDB ")
+}).catch((err)=>{
+    console.log(err);
+})
 
 
 
