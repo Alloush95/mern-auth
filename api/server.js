beaf1bc7220d0797ccc43fd6c5fc005ceeb607d9
Alloush95 <mohamad.alloush0090@stud.hkr.se>
2024-04-13 21:58:32 +0200

    create sign up api route

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import userRoite from './routes/user.route.js'
+import userRoute from './routes/user.route.js'
+import authRoute from './routes/auth.route.js'
 
 
 dotenv.config()
 const port = 3000;
 const app = express()
+app.use(express.json()); // to parse the incoming request with JSON payloads
 const passWord = process.env.PASS_WORD; // get the password from the .env file
 
 // Connect to MongoDB
@@ -24,4 +26,5 @@ app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 })
 
-app.use('/api/user', userRoite)  // use the user route
+app.use('/api/user', userRoute)  // use the user route
+app.use('/api/auth', authRoute)  // use the user route
